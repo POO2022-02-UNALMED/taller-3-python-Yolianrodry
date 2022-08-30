5f138adadc686709c8d9554ce33955c1ed3a0ed8
Yolian <105956749+Yolianrodry@users.noreply.github.com>
2022-08-30 00:26:02 -0500

    m4

--- Televisores/Marca.py
+++ Televisores/Marca.py
@@ -1,11 +1,10 @@
 #fff
 class Marca:
-
-    def _init_(self, nombre):
-        self._nombre = nombre
-
-    def setNombre(self, nombre):
+    def __init__(self, nombre):
         self._nombre = nombre
 
     def getNombre(self):
         return self._nombre
+
+    def setNombre(self, nombre):
+        self._nombre=nombre
