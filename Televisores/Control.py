b247d9e6ac80c5e3667aaecf78d14819c398d52e
Yolian <105956749+Yolianrodry@users.noreply.github.com>
2022-08-30 00:23:45 -0500

    c2

--- Televisores/Control.py
+++ Televisores/Control.py
@@ -1,36 +1,35 @@
 class Control:
-
-
-    def _init_(self):
+    
+    def __init__(self):
         self.tv = None
-
-    def turnOn(self):
-        self.tv.turnOn()
-
-    def turnOff(self):
-        self.tv.turnOff()
-
-    def canalUp(self):
-        self.tv.canalUp()
-
-    def canalDown(self):
-        self.tv.canalDown()
-
-    def volumenUp(self):
-        self.tv.volumenUp()
-
-    def volumenDown(self):
-        self.tv.volumenDown()
-
+    
     def setCanal(self, canal):
         self.tv.setCanal(canal)
-
-    def enlazar(self, tv):
-        self.tv = tv
-        tv.setControl(self)
-
+    
+    def turnOn(self):
+        self.tv.turnOn()
+        
+    def turnOff(self):
+        self.tv.turnOff()
+    
+    def canalUp(self):
+        self.tv.canalUp()
+        
+    def canalDown(self):
+        self.tv.canalDown()
+            
+    def volumenUp(self):
+        self.tv.volumenUp()
+        
+    def volumenDown(self):
+        self.tv.volumenDown()
+    
     def setTv(self, tv):
         self.tv = tv
-
+    
     def getTv(self):
         return self.tv
+    
+    def enlazar(self,tv):
+        self.tv = tv
+        tv.setControl(self)
